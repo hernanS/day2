2ecbacda53339e5c09858802c69b974e5bcc7a0e
unknown <yuxi@YUXMED0122L.internal.apexcorp.local>
2018-02-24 13:04:32 -0500

    day 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @Input() imageUrl = "https://api.adorable.io/avatars/500/300@adorable.io.png";
+  baseUrl= 'https://api.adorable.io/avatars/500/';
+  urlSuffix= '@adorable.io.png';
+  num;
+  constructor(){
+    setInterval( () =>{
+      this.num = Math.floor((Math.random() * 10) + 1);;
+      this.generateRandomFace(this.num);
+    }, 1000);
+  }
+
+  generateRandomFace(num) {
+    this.imageUrl = `${this.baseUrl}${this.num}${this.urlSuffix}`;
+  }
 }
